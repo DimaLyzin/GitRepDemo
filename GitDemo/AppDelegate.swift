77b28d44e8f13ebbf9111d1332255c198e6559d9
DimaLyzin <dmitri.lyzin@gmail.com>
2019-04-10 11:35:37 +0300

    Add new changes

--- GitDemo/AppDelegate.swift
+++ GitDemo/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
-        print("Hello World!")
+        print("Hello World New!")
         
         return true
     }
